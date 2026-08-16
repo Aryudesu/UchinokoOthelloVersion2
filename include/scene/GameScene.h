58139db0bf82e91c47e052fb12a4c62e794edcf0
Aryu <suwako_a-u-@hotmail.co.jp>
2026-08-16 22:32:12 +0900

    Add local game state

--- include/scene/GameScene.h
+++ include/scene/GameScene.h
@@ -7,11 +7,18 @@ class GameScene : public SceneBase {
     bool end_ = false;
     SceneID next_ = SceneID::Game;
     BitBoard board_;
+    Disc turn_ = Disc::Black;
+    Disc passed_ = Disc::Empty;
+    bool gameOver_ = false;
+    bool mouseLeftDown_ = false;
 
     static constexpr int BoardSize = BitBoard::Size;
     static constexpr int CellSize = 32;
     static constexpr int BoardLeft = 200;
     static constexpr int BoardTop = 80;
+
+    void handleBoardClick();
+    void advanceTurn();
 
 public:
     void Start() override;
