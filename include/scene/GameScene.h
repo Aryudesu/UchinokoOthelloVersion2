12ea9a995ba7f4a81f96b83ad45328d0901a2bba
Aryu <suwako_a-u-@hotmail.co.jp>
2026-08-17 01:00:28 +0900

    Run AI search in background

--- include/scene/GameScene.h
+++ include/scene/GameScene.h
@@ -5,8 +5,20 @@
 #include "ai/OthelloAI.h"
 
 #include <cstdint>
+#include <atomic>
+#include <chrono>
+#include <mutex>
+#include <optional>
+#include <thread>
 
 class GameScene : public SceneBase {
+    enum class Phase {
+        PlayerTurn,
+        AiThinking,
+        ApplyingAiMove,
+        GameOver,
+    };
+
     bool end_ = false;
     SceneID next_ = SceneID::Game;
     BitBoard board_;
@@ -20,6 +32,13 @@ class GameScene : public SceneBase {
     bool aiExactSearch_ = false;
     std::uint64_t aiTranspositionHits_ = 0;
     int aiIterations_ = 0;
+    Phase phase_ = Phase::PlayerTurn;
+    OthelloAI::SearchProgress aiProgress_;
+    std::optional<OthelloAI::Move> pendingAiMove_;
+    std::mutex aiResultMutex_;
+    std::atomic<bool> aiFinished_{ false };
+    std::chrono::steady_clock::time_point aiStartedAt_;
+    std::jthread aiThread_;
 
     static constexpr int BoardSize = BitBoard::Size;
     static constexpr int CellSize = 32;
@@ -29,8 +48,12 @@ class GameScene : public SceneBase {
     void handleBoardClick();
     void performAiMove();
     void advanceTurn();
+    void startAiSearch();
+    void finishAiSearch();
+    void cancelAiSearch();
 
 public:
+    ~GameScene() override;
     void Start() override;
     void End() override;
     void Update() override;
