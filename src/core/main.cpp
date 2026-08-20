ca2c40fe70ad1806a34c88e108115778cda2cafb
Aryu <suwako_a-u-@hotmail.co.jp>
2026-08-21 01:01:24 +0900

    描画画面を960x720へ拡張

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -17,6 +17,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ 
 	// DXライブラリ初期化
     SetOutApplicationLogValidFlag(FALSE);
     ChangeWindowMode(TRUE);
+    SetGraphMode(960, 720, 32);
     SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
     if (DxLib_Init() == -1) {
         MessageBox(NULL, "Dxlib Init Failed", "Error", MB_OK | MB_ICONERROR);
